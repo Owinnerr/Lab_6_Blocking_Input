6bfbab60355d2f0d2e678574f6521dc974d09f18
Owin Abeysinghe <owinabeysinghe@gmail.com>
2024-02-17 18:31:48 -0500

    Finished task 2

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -6,20 +6,20 @@ public class CtoFConverter {
         double celsiusTemperature = 0; // Initialize celsius variable (for user input)
         double fahrenheitTemperature = 0; // Initialize fahrenheit variable (conversion)
         String trash = ""; // Initialize trash variable (bad user input)
-        boolean done = false; // Initialize done variable and set to false for do while loop[
+        boolean done = false; // Initialize done variable and set to false for do while loop
         do {
             System.out.print("Enter a temperature in celsius: "); // Prompt user for temperature in celsius
             if (in.hasNextDouble()) { // Condition: If it's safe to read in a double
                 celsiusTemperature = in.nextDouble(); // Reads a double value from the user
                 in.nextLine(); // Clears the newline from the buffer
-                fahrenheitTemperature = 1.8 * (celsiusTemperature) + 32; // Change the variable to the celsius to fahrenheit equation
                 done = true; // Set done to true since it is a valid number, and therefore we can end the loop
-            } else { //
+            } else {
                 trash = in.nextLine(); // Not a double so read the user input as a string
                 System.out.println("You said the temperature was " + trash + ", which is not a valid temperature."); // Output that the user input is not a valid temperature
                 System.out.println("You have to enter a valid temperature."); // Output that the user should enter a valid temperature
             }
         } while(!done); // Loop: While done variable is false (loop will stop when done is true, or in other words once the user enters a valid temp.)
+        fahrenheitTemperature = 1.8 * (celsiusTemperature) + 32; // Set the variable to the celsius to fahrenheit equation
         System.out.println("The temperature in fahrenheit is " + fahrenheitTemperature); // Output the temperature in fahrenheit
     }
 }
---
+++ src/FuelCosts.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class FuelCosts {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double numberOfGallons = 0; // Initialize number of gallons variable
+        double fuelEfficiency = 0; // Initialize fuel efficiency variable
+        double priceOfGas = 0; // Initialize price of gas variable
+        double costPerHundredMiles = 0; // Initialize cost per hundred miles variable
+        double carRangeWithGas = 0; // Initialize car range variable
+        String trash = ""; // Initialize trash variable
+        boolean done1 = false; // Initialize done1 variable (initially set as false for while loop)
+        boolean done2 = false; // Initialize done2 variable (initially set as false for while loop)
+        boolean done3 = false; // Initialize done3 variable (initially set as false for while loop)
+        do {
+            System.out.print("Enter the number of gallons of gas in the tank: "); // Prompt the user for the number of gallons of gas
+            if (in.hasNextDouble()) { // Condition: If it's safe to read in a double
+                numberOfGallons = in.nextDouble(); // Reads a double value from the user
+                in.nextLine(); // Clears the newline from the buffer
+                done1 = true; // Set done1 to true since it is a valid number, and therefore we can end the loop
+            } else {
+                trash = in.nextLine(); // Not a double so read the user input as a string
+                System.out.println("You said the number of gallons was " + trash + ", which is not a valid number."); // Output that the user input is not a valid number
+                System.out.println("You have to enter a valid number."); // Output that the user should enter a valid number
+            }
+        } while(!done1); // Loop: While done1 variable is false (loop will stop when done1 is true, or in other words once the user enters a valid number)
+        do {
+            System.out.print("Enter the fuel efficiency in miles per gallon: "); // Prompt the user for the fuel efficiency
+            if (in.hasNextDouble()) { // Condition: If it's safe to read in a double
+                fuelEfficiency = in.nextDouble(); // Reads a double value from the user
+                in.nextLine(); // Clears the newline from the buffer
+                done2 = true; // Set done2 to true since it is a valid number, and therefore we can end the loop
+            } else {
+                trash = in.nextLine(); // Not a double so read the user input as a string
+                System.out.println("You said the fuel efficiency was " + trash + ", which is not a valid number."); // Output that the user input is not a valid number
+                System.out.println("You have to enter a valid number."); // Output that the user should enter a valid number
+            }
+        } while(!done2); // Loop: While done2 variable is false (loop will stop when done2 is true, or in other words once the user enters a valid number)
+        do {
+            System.out.print("Enter the price of gas per gallon: "); // Prompt the user for the price of gas
+            if (in.hasNextDouble()) { // Condition: If it's safe to read in a double
+                priceOfGas = in.nextDouble(); // Reads a double value from the user
+                in.nextLine(); // Clears the newline from the buffer
+                done3 = true; // Set done3 to true since it is a valid number, and therefore we can end the loop
+            } else {
+                trash = in.nextLine(); // Not a double so read the user input as a string
+                System.out.println("You said the price of gas per gallon " + trash + ", which is not a valid number."); // Output that the user input is not a valid number
+                System.out.println("You have to enter a valid number."); // Output that the user should enter a valid number
+            }
+        } while(!done3); // Loop: While done3 variable is false (loop will stop when done3 is true, or in other words once the user enters a valid number)
+        costPerHundredMiles = priceOfGas * 100.0; // Set the variable to the price of gas multiplied by 100
+        System.out.println("The cost per 100 miles is $" + costPerHundredMiles); // Output the cost per 100 miles
+        carRangeWithGas = numberOfGallons * fuelEfficiency; // Set the variable to number of gallons multiplied by fuel efficiency
+        System.out.println("The amount of miles the car can go with the gas in the tank is " + carRangeWithGas); // Output the car range using the gas in the tank
+    }
+}
