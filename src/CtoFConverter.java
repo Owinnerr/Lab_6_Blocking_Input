5d891a06aa5db572dfb7db97e8fba5b55960eee7
Owin Abeysinghe <owinabeysinghe@gmail.com>
2024-02-17 16:40:42 -0500

    Finished task 1

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -3,16 +3,23 @@ import java.util.Scanner;
 public class CtoFConverter {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int celsiusTemperature = 0;
-        int fahrenheitTemperature = 0;
-        String trash = "";
-        boolean done = false;
+        double celsiusTemperature = 0; // Initialize celsius variable (for user input)
+        double fahrenheitTemperature = 0; // Initialize fahrenheit variable (conversion)
+        String trash = ""; // Initialize trash variable (bad user input)
+        boolean done = false; // Initialize done variable and set to false for do while loop[
         do {
-            System.out.println("Enter a temperature in celsius: ");
-            if (in.hasNextInt()) {
-                celsiusTemperature = in.nextInt();
-                in.nextLine();
+            System.out.print("Enter a temperature in celsius: "); // Prompt user for temperature in celsius
+            if (in.hasNextDouble()) { // Condition: If it's safe to read in a double
+                celsiusTemperature = in.nextDouble(); // Reads a double value from the user
+                in.nextLine(); // Clears the newline from the buffer
+                fahrenheitTemperature = 1.8 * (celsiusTemperature) + 32; // Change the variable to the celsius to fahrenheit equation
+                done = true; // Set done to true since it is a valid number, and therefore we can end the loop
+            } else { //
+                trash = in.nextLine(); // Not a double so read the user input as a string
+                System.out.println("You said the temperature was " + trash + ", which is not a valid temperature."); // Output that the user input is not a valid temperature
+                System.out.println("You have to enter a valid temperature."); // Output that the user should enter a valid temperature
             }
-        } while (!done);
+        } while(!done); // Loop: While done variable is false (loop will stop when done is true, or in other words once the user enters a valid temp.)
+        System.out.println("The temperature in fahrenheit is " + fahrenheitTemperature); // Output the temperature in fahrenheit
     }
 }
